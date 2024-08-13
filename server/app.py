4a1a896ccd3611cf94de6ad5e3f55220d68ba1be
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-08-13 14:53:52 -0400

    first commit

--- server/app.py
+++ server/app.py
@@ -46,6 +46,20 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+        plant.is_in_stock = data['is_in_stock']
+        db.session.commit()
+        return plant.to_dict(), 200
+    
+    def delete(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return (), 204
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
